b490545cf624ddc9c6fd7f8c5d4f0430b8aaaec1
Fineas <r0845664@student.thomasmore.be>
2023-09-19 23:26:46 +0200

    task tracker console aplicatie in node.js die in c# moet omgezet worden. dal en models files aangemaakt, database conection aangemaakt en bqse
    bq
    baserepository aangemaakt

---
+++ Console/dal/BaseRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace dal
+{
+    public class BaseRepository
+    {
+        protected string ConnectionString { get; }
+
+        public BaseRepository()
+        {
+            ConnectionString = DatabaseConection.ConnectionString("tasks");
+        }
+    }
+}
